c8511a47a2f8f10a5b3440bf34a9147c74d61920
Shravani Patil <meetpatilshravani23@gmail.com>
2025-07-28 23:58:24 +0530

    dummy-auth

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
 const express=require("express");
 const app=express();
+const {isAuthenticate} =require("./middlewares/auth");
 
 module.exports =app;
+app.get("/user",isAuthenticate);
+
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(401).end("Something went wrong");
+    }
+})
---
+++ src/middlewares/auth.js
@@ -0,0 +1,13 @@
+const isAuthenticate=(req,res,next)=>{
+const token="uwcugcdggdhd";
+const Authenticated = token==="uwcugcdggdhd";
+if(!Authenticated){
+res.status(401).send("error");
+}
+else{
+    next();
+}
+
+
+}
+module.exports={isAuthenticate}
