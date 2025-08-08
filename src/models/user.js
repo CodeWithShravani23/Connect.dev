851af918dca14a724faec61ac6ba8675fe049e45
Shravani Patil <meetpatilshravani23@gmail.com>
2025-08-08 23:59:09 +0530

    data validation

--- src/models/user.js
+++ src/models/user.js
@@ -3,21 +3,26 @@ const { Schema } = require("mongoose");
 const userSchema = new Schema({
   firstName: {
     type: String,
+    required:true
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+     required:true
   },
   password: {
     type: String,
+     required:true
   },
   age: {
     type: Number,
+     required:true
   },
   gender: {
     type: String,
+     required:true
   },
 });
 const UserData = mongoose.model("User", userSchema);
