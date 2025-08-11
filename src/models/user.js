ca8c9fb2227040fb0b03667e072388d193150908
Shravani Patil <meetpatilshravani23@gmail.com>
2025-08-11 17:25:41 +0530

    added some more validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,49 +1,56 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 2,
-    maxLength: 50,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      minLength: 2,
+      maxLength: 50,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+      maxLength: 15,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
+      max: 90,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female", "other"],
+    },
+    profilePhoto: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg",
+    },
+    about: {
+      type: String,
+      maxLength: 300,
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-    min: 18,
-    lowercase: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-    enum: ["male", "female", "other"],
-  },
-  profilePhoto: {
-    type: String,
-    default:
-      "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg",
-  },
-  about: {
-    type: String,
-  },
-  skills:{
-    type:[String]
-  }
-},
-{ timestamps: true });
+  { timestamps: true }
+);
 const UserData = mongoose.model("User", userSchema);
 module.exports = UserData;
