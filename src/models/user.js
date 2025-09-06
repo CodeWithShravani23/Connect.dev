fde92327095ab8c4dcc389f3485226fb770717da
Shravani Patil <meetpatilshravani23@gmail.com>
2025-09-06 12:58:45 +0530

    .

--- src/models/user.js
+++ src/models/user.js
@@ -73,7 +73,7 @@ const userSchema = new Schema(
 
 userSchema.methods.getJWT = async function () {
   const user = this;
-  let token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+  let token = await jwt.sign({ _id: user._id },process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
   return token;
